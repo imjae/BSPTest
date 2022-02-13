a8a66bae401626affa24ba06b825f3aecaf03cad
imjae <code@kakao.com>
2022-02-13 10:00:57 +0900

    vs코드 디버깅툴 성공

--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+    public int a, b, c;
     // Start is called before the first frame update
     void Start()
     {
-        
+        a = 1;
+        b = 2;
+        c = 3;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
